c41c655687a598c095e4dba74788cd8c4ea37611
Jakub Kadzielawa <jakub.kadzielawa@aexp.com>
2026-09-15 13:33:32 +0200

    update

---
+++ functions/[[path]].ts
@@ -0,0 +1,21 @@
+export async function onRequest(context: PagesFunction) {
+  const incoming = new URL(context.request.url);
+
+  const upstream = new URL(incoming);
+  upstream.protocol = "https:";
+  upstream.hostname = "rooampay.americanexpress.com";
+
+  const headers = new Headers(context.request.headers);
+  headers.set("Host", "rooampay.americanexpress.com");
+
+  return fetch(upstream, {
+    method: context.request.method,
+    headers,
+    body:
+      context.request.method === "GET" ||
+      context.request.method === "HEAD"
+        ? undefined
+        : context.request.body,
+    redirect: "manual",
+  });
+}
